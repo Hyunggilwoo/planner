e0aaf200b8d4c0c187a7893613625d66d929eb6e
hyunggilwoo <hyunggilswoo@gmail.com>
2023-07-21 14:26:54 -0700

    Created a script to generate a quarterly planner.
    What to work on: Currently, it prints a quarterly planner starting on current month that one prints it.
    Also, it prints 'The Week Ahead' page based on the first day of the month that the page prints it.
    
    I will try to fix it.

--- monthly_planner.rb
+++ monthly_planner.rb
@@ -18,64 +18,85 @@ end
 def calculate_dates
     date = ARGV.empty? ? Date.today : Date.parse(ARGV.first)
 
-    sunday = if ARGV.empty? # if no argument, generate pages for set time
-        date = DateTime.now.to_date
-        if date.wday > 2
-          puts "Generating pages for the next week"
-          date.next_day(7-date.wday)
-        else
-          puts "Generating pages for this week"
-          date.prev_day(date.wday)
+    start_date = Date.new(date.year, date.month - 1)
+    end_date = Date.new(date.year, date.month, -1)
+
+    (start_date..end_date)
+end
+
+def calculate_quarterly_dates
+    date = ARGV.empty? ? Date.today : Date.parse(ARGV.first)
+
+    quarter = ((date.month - 1) / 3) + 1
+    quarter_start_month = (quarter - 1) * 3 + 1
+
+    start_date = Date.new(date.year, quarter_start_month)
+    end_date = (start_date >> 2) - 1
+
+    [start_date, end_date]
+end
+
+
+
+def generate_quarterly_pages(start_date, end_date)
+    '''
+    Generate monthly pages. Assumes that a quarter is 13 weeks.
+    pre: start date and end_date is given
+    post:
+    '''
+    quarter_start = start_date
+    quarter_end = end_date
+    week_start = quarter_start
+
+    # which part are we comparing?
+    while week_start <= quarter_end
+        week_end = week_start + 6
+
+        begin_new_page :right
+
+        week_ahead_page(week_start, week_end) # week_ahead_page accepts 2 parameters
+
+        puts "Generate pages for a quarter of #{week_start.strftime('%A, %B %-d, %Y')}: in #{FILE_NAME}"
+
+        # Generate days in a week
+        (week_start..week_end).each do |date|
+            # ...then the page for the week
+
+            # I want to measure my weekend too
+            daily_tasks_page date
+            daily_calendar_page date
+            journal_page date
+
         end
-      else
-        date = DateTime.parse(ARGV.first).to_date # if date specified, generate pages for the specified time
-        puts "Parsed #{date} from arguments"
-        date.prev_day(date.wday)
-      end
-      sunday
+        # update new week_start
+        week_start = week_end + 1
     end
 end
 
-def generate_weekly_pages(sunday)
-    '''
-    Generate monthly pages.
-    '''
-    WEEKS.times do |week|
-        unless week.zero?
-          # ...then the page for the week
-          begin_new_page :right
-        end
-    
-        monday = sunday.next_day(1)
-        # sunday = sunday.next_day(7)
-        puts "Generate pages for week #{monday.strftime('%W')}: #{monday.strftime('%A, %B %-d, %Y')} through #{sunday.strftime('%A, %B %-d, %Y')} in #{FILE_NAME}"
-        week_ahead_page monday, sunday
-    
-        # I want to measure my weekend too
-        (1..7).each do |i|
-          day = sunday.next_day(i)
-          daily_tasks_page day
-          daily_calendar_page day
-          journal_page day
-        end
-    
-        sunday = monday.next_day(6)
-    end
-end
-
-date = ARGV.empty? ? Date.today : Date.parse(ARGV.first)
-start_date = Date.new(date.year, date.month - 1)
-end_date = Date.new(date.year, date.month, -1)
+# date = ARGV.empty? ? Date.today : Date.parse(ARGV.first)
+# start_date = Date.new(date.year, date.month - 1)
+# end_date = Date.new(date.year, date.month, -1)
 
 # TODO: Can I change the file name later after a user prompt?
-MONTHLY_FILE_NAME = ""
+def journal_title(start_date, end_date)
+    return title = "#{start_date.year} #{start_date.strftime('%B')} - #{end_date.strftime('%B')} Timeblock Planner"
+end
+
+# Journal titles are changed here
+start_date, end_date = calculate_quarterly_dates
+
+QUARTERLY_FILE_NAME = journal_title(start_date, end_date)
+
+# Journal title ends here
+
 '''
 Generating a monthly document
 User enters a year and month, it generates a monthly calendar page
+post: generate a single file of all pages combined
 '''
-Prawn::Document.generate(FILE_NAME, margin: RIGHT_PAGE_MARGINS, print_scaling: :none) do
+Prawn::Document.generate(QUARTERLY_FILE_NAME, margin: RIGHT_PAGE_MARGINS, print_scaling: :none) do
     setup
-    sunday = calculate_dates
-    generate_weekly_pages(sunday)
+    start_date, end_date = calculate_quarterly_dates
+    generate_quarterly_pages(start_date, end_date)
 end
       
